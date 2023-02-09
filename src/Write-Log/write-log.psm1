da1e08d51184a30d3cd5026b27eface4f3398204
Torben Inselmann <Torben@inselmann.de>
2023-02-09 21:42:25 +0100

    Rename & Change of the initial logfile creation

--- src/Write-Log/write-log.psm1
+++ src/Write-Log/write-log.psm1
@@ -4,11 +4,11 @@
 .SYNOPSIS
     <Providing a log function in Powershell>
 .DESCRIPTION
-    <Provides a log function which stores a log under $Env:TEMP\myPosh, this function can be called from other scripts.
+    <Provides a log function which stores a log under $env:SystemDrive\tmp\myPosh_Log, this function can be called from other scripts.
     as status level 'Information', 'Warning', 'Error' and 'Debug' can be used.
     If the parameter "-console $true" is passed the corresponding message will also be printed on the console and colored according to the status level.>
 .OUTPUTS
-    <C:\tmp\Powershell\PS_log.txt>
+    <Output to e.g. the following file C:\tmp\myPosh_Log\20230209_logfile.txt or on the console "20230209-21:34:42 | Error | Test message".>
 .FUNCTIONALITY
     <Takes information in a script and stores it with time stemple in a log file in addition, the -console $true parameter can also be used to display output in the console >
 .EXAMPLE
@@ -19,17 +19,21 @@
     Git: https://github.com/nox309
     DateCreated: 2022/12/23
     #>
-$logpath = "$Env:TEMP\myPosh"
+$date = (get-date -Format yyyyMMdd)
+$filename = $date + "_logfile.txt"
+$logpath = "$env:SystemDrive\tmp\myPosh_Log\"
+$log = $logpath + $filename
 
-if(Test-Path  $logpath) 
-	{ 
-	Write-Host "Log Pfad gefunden"
-	}  
-else
+if(!(Test-Path  $logpath)) 
 	{
-	  mkdir $logpath
-	  Write-Host "Log Pfad erstellt"
+        mkdir $logpath
 	}
+if(!(Test-Path  $log))
+    {
+        "Timestamp | Severity | Message" | Out-File -FilePath $log -Append -Encoding utf8
+        "$(get-date -Format yyyyMMdd-HH:mm:ss) | Information | Log started" | Out-File -FilePath $log -Append -Encoding utf8
+    }
+
 function Write-Log {
     [CmdletBinding()]
     param(
@@ -47,11 +51,6 @@ function Write-Log {
     )
 
     $time = (get-date -Format yyyyMMdd-HH:mm:ss)
-
-    if (!(Test-Path $logpath\myPosh.txt)) {
-        "Timestamp | Severity | Message" | Out-File -FilePath $logpath\myPosh.txt -Append  -Encoding utf8
-        "$Time | Information | Log started" | Out-File -FilePath $logpath\myPosh.txt -Append  -Encoding utf8
-    }
 
     if ($console) {
         if ($Severity -eq "Information") {
@@ -73,6 +72,6 @@ function Write-Log {
         Write-Host -ForegroundColor $color "$Time | $Severity | $Message"
     }
 
-    "$Time | $Severity | $Message" | Out-File -FilePath $logpath\myPosh.txt -Append  -Encoding utf8
+    "$Time | $Severity | $Message" | Out-File -FilePath $log -Append -Encoding utf8
 
 }
